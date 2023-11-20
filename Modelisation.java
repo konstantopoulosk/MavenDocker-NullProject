d41e66dc7226069de8f810a6a02dbf16cc1d619e
MariaTsoulakou <148355474+MariaTsoulakou@users.noreply.github.com>
2023-11-20 17:55:25 +0200

    Update Modelisation.java

--- Modelisation.java
+++ Modelisation.java
@@ -9,13 +9,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Modelisation {
-  //  private DockerClient dockerClient = DockerClientBuilder.getInstance().build(); //Client
-  //  private List<Image> images = dockerClient.listImagesCmd().exec(); //List images tou client
-  //  private List<java.awt.Container> containers = dockerClient.listContainersCmd().exec(); // list containers tou client
-
     private DockerClient dockerClient;
-    private List<Image> images;
-    private List<java.awt.Container> containers;
+    List<Image> images;
+    List<java.awt.Container> containers;
 
     //constructor for image methods
     Modelisation(DockerClient dockerClient, List<Image> images) {
@@ -50,6 +46,7 @@ public class Modelisation {
         System.out.println("Or press 0 to go back");
         int c = in.nextInt();
         if (c != 0) {
+            String id = containers.get(c-1).getId(); //container's id, parameter in executor
             char a = in.next().charAt();
             switch (a) {
                 case 'S':
@@ -86,6 +83,7 @@ public class Modelisation {
         int c = in.nextInt();
         if (c != 0) {
             char a = in.next().charAt();
+            String id = containers.get(c-1).getId(); //container's id, parameter in executor
             switch (a) {
                 case 'R':
                     //executor
