d3bea8f65932b5bf26155e4de1c906f1dec21a4a
AnastasiaLag <147436031+AnastasiaLag@users.noreply.github.com>
2023-12-02 03:18:37 +0200

    Update DockerImage.java

--- dockermonitor/src/main/java/com/nullteam/DockerImage.java
+++ dockermonitor/src/main/java/com/nullteam/DockerImage.java
@@ -2,21 +2,24 @@ package com.nullteam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 public class DockerImage {
     private static List<DockerImage> all_images = new ArrayList<>(); //Lista me oles tis eikones
-    private String imageId;
-    private String imageName;
-    private String imageTag;
+    private final String imageRep;                    // These fields won't ever change
+    private final String imageTag;                    // That's why they're final
+    private final String imageId;
 
-    public DockerImage(String imageId, String imageName, String imageTag) {
-        this.imageId = imageId;
-        this.imageName = imageName;
+    public DockerImage(String imageRep, String imageTag, String imageId) {
+        this.imageRep = imageRep;
         this.imageTag = imageTag;
+        this.imageId = imageId;
         all_images.add(this);
     }
     //getters
-    public String getImageName() {
-        return imageName;
+    public String getImageRep() {
+        String[] parts = this.imageRep.split("@");
+        return parts[0]; //to keep only the name of the repository
     }
     public String getImageTag() {
         return imageTag;
@@ -26,6 +29,27 @@ public class DockerImage {
     }
     @Override
     public String toString() {
-        return "Image ID: " + imageId + "\nImage Name: " + imageName + "\nImage Tag: " + imageTag;
+        return "REPOSITORY: " + getImageRep() +  "  TAG: " + imageTag + "  IMAGE ID: " + imageId ;
     }
+
+    //helpful methods
+    
+    //this method prints all images with numbers in the beginning
+    public static void listAllImages() {
+        System.out.println("Listing all the images...\n.\n.\n.");
+        int num = 0;
+        for (DockerImage img : all_images) {
+            num++;
+            System.out.println(num + ") " + img.toString());
+        }
+    }
+    //this method helps to match the numbers form the printed images with their id
+    public static String chooseAnImage() {
+        DockerImage.listAllImages();
+        Scanner in = new Scanner(System.in);
+        int choice = in.nextInt();
+        return all_images.get(choice - 1).getImageId();
+    }
+    
+    //public static void implementAnImage(String imageId) {}  obviously not done yet...
 }
