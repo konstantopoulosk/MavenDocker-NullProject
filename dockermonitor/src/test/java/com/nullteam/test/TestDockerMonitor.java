c9fc443bdb8d81f87cf4c52c996f82e7d2c163a3
Grigorios Alexandrou <t8220004@aueb.gr>
2023-12-02 18:00:48 +0200

    TODO: Complete TestDockerMonitor

--- dockermonitor/src/test/java/com/nullteam/test/TestDockerMonitor.java
+++ dockermonitor/src/test/java/com/nullteam/test/TestDockerMonitor.java
@@ -1,5 +1,28 @@
 package com.nullteam.test;
 
+import com.nullteam.DockerInstance;
+import com.nullteam.DockerMonitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 public class TestDockerMonitor {
-    
+    private List<String[]> containers = new ArrayList<>();
+    private String name = "Name";
+    private String id = "Id";
+    private String image = "image";
+    private String status = "Up";
+    String[] array = { name , id , image , status };
+    @Before
+    public void setUp() {
+        containers.add(array);
+    }
+    @Test
+    public void testHasNewData() {
+        String[] container2 = {"Name1" , "Id", "image", "status"};
+        List<String[]> array2 = new ArrayList<>();
+        array2.add(container2);
+        
+    }
 }
