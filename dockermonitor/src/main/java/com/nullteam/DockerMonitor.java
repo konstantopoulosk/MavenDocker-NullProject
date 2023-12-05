da2efa3f6698e54025ac6a1ccfebe3f34a66af27
Grigorios Alexandrou <t8220004@aueb.gr>
2023-12-06 00:56:30 +0200

    CheckStyle finished in DockerMonitor

--- dockermonitor/src/main/java/com/nullteam/DockerMonitor.java
+++ dockermonitor/src/main/java/com/nullteam/DockerMonitor.java
@@ -9,20 +9,40 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class DockerMonitor extends Thread {
+    /**
+     * List of all Container info,
+     * in last state.
+     */
     private List<String[]> lastState = null;
+    /**
+     * List of all Container info,
+     * currently.
+     */
     private List<String[]> currentData = null;
 
+    /**
+     * Method run to execute Monitor Thread.
+     */
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-            if(hasNewData()) {
+            if (hasNewData()) {
                 writeCsv();
             }
         }
     }
+
+    /**
+     * Method writeCsv writes in containers.csv,
+     * info of every container.
+     * Container ID, Name, Image,
+     * Status, Command, Created.
+     */
     public void writeCsv() { //Write/update the csv file
         final String csvFilePath = "containers.csv";
-        try(CSVWriter csvWriter = new CSVWriter(new FileWriter(csvFilePath, false))){
-            csvWriter.writeNext(new String[]{"Container ID","Name", "Image", "Status", "Command", "Created"}); // CSVFile header
+        try (CSVWriter csvWriter = new CSVWriter(
+                new FileWriter(csvFilePath, false))) {
+            csvWriter.writeNext(new String[]{"Container ID", "Name",
+                   "Image", "Status", "Command", "Created"}); // CSVFile header
             for (String[] csvData : currentData) {
                 csvWriter.writeNext(csvData);
             }
@@ -37,8 +57,16 @@ public class DockerMonitor extends Thread {
             e.printStackTrace();
         }
     }
-    public boolean hasNewData(){ // Check if there is any change inside the cluster
-        List<Container> containers = ClientUpdater.getUpdatedContainersFromClient();
+
+    /**
+     * Method hasNewData checks if,
+     * container info has changed.
+     * @return boolean
+     */
+    public boolean hasNewData() {
+        //Check if there is any change inside the cluster
+        List<Container> containers =
+                ClientUpdater.getUpdatedContainersFromClient();
         currentData = new ArrayList<>();
         for (Container c : containers) {
             String[] csvData = new String[]{
@@ -58,7 +86,17 @@ public class DockerMonitor extends Thread {
             return false;
         }
     }
-    public boolean listsAreEqual(List<String[]> list1, List<String[]> list2) {
+
+    /**
+     * Method listsAreEqual checks if,
+     * two lists are equal.
+     * Current Data and Last State.
+     * @param list1
+     * @param list2
+     * @return boolean
+     */
+    public boolean listsAreEqual(final List<String[]> list1,
+                                 final List<String[]> list2) {
         // Check for null references
         if (list1 == null && list2 == null) {
             return true;
