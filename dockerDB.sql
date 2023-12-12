28f170078d165ec8be94d1d3a524464781cb6d7f
Anastasia Lagopanagiotopoulou <t8220075@aueb.gr>
2023-12-12 21:13:43 +0200

    fixed db

--- dockerDB.sql
+++ dockerDB.sql
@@ -2,7 +2,8 @@ create database dockerDB;
 use dockerDB;
 
 /*drop table Measurements
-  drop table DockerInstance*/
+  drop table DockerInstance
+  drom table DockerImage*/
   
 create table Measurements(
 id int not null auto_increment primary key,
@@ -10,10 +11,18 @@ date date not null
 );
 create table DockerInstance(
 id varchar(64) not null primary key,
-name varchar(100) not null,
-image varchar(100),
-state varchar(10)not null,
+name varchar(100),
+image varchar(100) not null,
+state varchar(10) not null,
 command varchar(100) not null,
-created varchar(10),
+created varchar(10) not null,
 ports varchar(8) not null
 );
+create table DockerImage(
+id varchar(71) not null primary key,
+repository varchar(100) not null,
+tag varchar(100),
+timesUsed varchar(5),
+size varchar(10)
+)
+
