619875d8c3e3e7f41de8b581ac51365f892a39b6
Anastasia Lagopanagiotopoulou <t8220075@aueb.gr>
2023-12-19 22:58:08 +0200

    testDockerVolume sxedon

--- dockermonitor/src/test/java/com/nullteam/test/TestDockerImage.java
+++ dockermonitor/src/test/java/com/nullteam/test/TestDockerImage.java
@@ -10,57 +10,57 @@ import org.junit.Test;
 import java.util.*;
 
 public class TestDockerImage {
-    private static List<com.nullteam.DockerImage> allImages;
+    private static List<com.nullteam.DockerImage> imageslist;
     private final String imageId = "593aee2afb64";
     private final String imageName = "nginx";
-    private final String imageTag = "Latest";
+    private final String imageTag = "latest";
     private final DockerImage dockerImage = new DockerImage(imageName,imageTag,imageId);
     private final DockerImage di = new DockerImage("Gregory", "Latest", "123456");
 
     @Before
     public void setUp() { //FINISHED
-        allImages = new ArrayList<>();
-        allImages.add(dockerImage);
-        allImages.add(di);
+        imageslist = new ArrayList<>();
+        imageslist.add(dockerImage);
+        imageslist.add(di);
     }
     @Test
     public void testGetImageRep() {
-        Assert.assertEquals("Failure - wrong Name" , allImages.get(0).getImageRep(), "nginx");
+        Assert.assertEquals("Failure - wrong Name" , imageslist.get(0).getImageRep(), "nginx");
     }
     @Test
     public void testGetImageTag() {
-        Assert.assertEquals("Failure - wrong Tag", allImages.get(0).getImageTag(), "Latest");
+        Assert.assertEquals("Failure - wrong Tag", imageslist.get(0).getImageTag(), "Latest");
     }
     @Test
     public void testGetImageId() throws NullPointerException {
         Assert.assertEquals("Failure - wrong Id",
-                allImages.get(0).getImageId(),
+                imageslist.get(0).getImageId(),
                 "593aee2afb64");
     }
     @Test
     public void testToString() {
-        Assert.assertEquals("Failure wrong to String", allImages.get(0).toString(),dockerImage.toString());
+        Assert.assertEquals("Failure wrong to String", imageslist.get(0).toString(),dockerImage.toString());
     }
     @Test
     public void testConstructor() {
-        Assert.assertEquals("Failure wrong size", allImages.size(), 2);
-        Assert.assertEquals("Failure wrong First", allImages.get(0).getImageId(), imageId);
+        Assert.assertEquals("Failure wrong size", imageslist.size(), 2);
+        Assert.assertEquals("Failure wrong First", imageslist.get(0).getImageId(), imageId);
     }
     @Test
     public void testListAllImages() { //FINISHED
         String actual = "1) REPOSITORY: nginx  TAG: Latest  IMAGE ID: 593aee2afb64" +
                 "\n2) REPOSITORY: Gregory  TAG: Latest  IMAGE ID: 123456";
-        String expected = "1) " + allImages.get(0).toString() + "\n2) "
-                + allImages.get(1).toString();
+        String expected = "1) " + imageslist.get(0).toString() + "\n2) "
+                + imageslist.get(1).toString();
         Assert.assertEquals("Fail wrong output", expected,actual);
     }
     @Test
     public void testChooseAnImage() {
-        Assert.assertTrue("Fail", allImages.contains(dockerImage));
-        Assert.assertTrue("Fail", allImages.contains(di));
+        Assert.assertTrue("Fail", imageslist.contains(dockerImage));
+        Assert.assertTrue("Fail", imageslist.contains(di));
         DockerImage dockerImage1 = new DockerImage("REP",
                 "TAG", "ID");
-        Assert.assertFalse("Fail", allImages.contains(dockerImage1));
+        Assert.assertFalse("Fail", imageslist.contains(dockerImage1));
     }
     @Test
     public void testImplementAnImage() {
@@ -68,6 +68,6 @@ public class TestDockerImage {
     }
     @After //FINISHED
     public void tearDown() {
-        allImages = null;
+        imageslist = null;
     }
 }
---
+++ dockermonitor/src/test/java/com/nullteam/test/TestDockerVolume.java
@@ -0,0 +1,81 @@
+package com.nullteam.test;
+
+import com.nullteam.DockerVolume;
+import org.jetbrains.annotations.NotNull;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+public class TestDockerVolume {
+    private static List<DockerVolume> volumeslist;
+    private final String driver = "local";
+    private final String name = "02cf53413ed35002a1a56531e43feda3ac39e029fbe635a0cca0cf30cab21a59";
+    private final String created = "2023-11-15T18:33:33Z";
+    private final String mountpoint = "/var/lib/docker/volumes/02cf53413ed35002a1a56531e43feda3ac39e029fbe635a0cca0cf30cab21a59/_data";
+    private final DockerVolume dockerVolume = new DockerVolume(driver, name, created, mountpoint);
+    private final DockerVolume dv = new DockerVolume("other",
+            "414778454d1b3dfdaf3c58249c8dee5fab72c4d528ff1650544cecb1323d8011", "2022-11-12T11:11:11Z",
+            "/var/lib/docker/volumes/414778454d1b3dfdaf3c58249c8dee5fab72c4d528ff1650544cecb1323d8011/_data");
+    @Before
+    public void setUp() {
+        volumeslist = new ArrayList<>();
+        volumeslist.add(dockerVolume);
+        volumeslist.add(dv);
+    }
+    @Test
+    public void testConstructor() {
+        Assert.assertEquals("Failure wrong size", volumeslist.size(), 2);
+        Assert.assertEquals("Failure wrong First", volumeslist.getFirst().getName(), name);
+    }
+    @Test
+    public void testGetDriver() {
+        Assert.assertEquals("Failure - wrong Driver",
+                volumeslist.getFirst().getDriver(), "local");
+    }
+    @Test
+    public void testGetName() {
+        Assert.assertEquals("Failure - wrong Name",
+                volumeslist.getFirst().getName(),
+                "02cf53413ed35002a1a56531e43feda3ac39e029fbe635a0cca0cf30cab21a59");
+    }
+    @Test
+    public void testGetCreated() {
+        Assert.assertEquals("Failure - wrong Created",
+                volumeslist.getFirst().getCreated(),
+                "2023-11-15T18:33:33Z");
+    }
+    @Test
+    public void testGetMountpoint() {
+        Assert.assertEquals("Failure - wrong Mountpoint",
+                volumeslist.getFirst().getMountpoint(),
+                "/var/lib/docker/volumes/02cf53413ed35002a1a56531e43feda3ac39e029fbe635a0cca0cf30cab21a59/_data");
+    }
+    @Test
+    public void testToString() {
+        Assert.assertEquals("Failure wrong to String",
+                volumeslist.getFirst().toString(),
+                dockerVolume.toString());
+    }
+    @Test
+    public void testShowVolumes() {
+        String actual = "1) Driver: local Name: 02cf53413ed35002a1a56531e43feda3ac39e029fbe635a0cca0cf30cab21a59"
+                + " CreatedAt: 2023-11-15T18:33:33Z"
+                + " Mountpoint: /var/lib/docker/volumes/02cf53413ed35002a1a56531e43feda3ac39e029fbe635a0cca0cf30cab21a59/_data"
+                + "2) Driver: other Name: 414778454d1b3dfdaf3c58249c8dee5fab72c4d528ff1650544cecb1323d8011\n"
+                + " CreatedAt: 2022-11-12T11:11:11Z"
+                + " Mountpoint: /var/lib/docker/volumes/414778454d1b3dfdaf3c58249c8dee5fab72c4d528ff1650544cecb1323d8011/_data\n";
+        String expected = "1) " + volumeslist.getFirst().toString() + "\n\n"
+                + "2) " + volumeslist.get(1).toString() + "\n";
+        Assert.assertEquals("Fail wrong output", expected,actual);
+    }
+    @Test
+    public void testCreatedAt() {
+        //no idea what  do to :/
+    }
+    @After
+    public void tearDown() {
+        volumeslist = null;
+    }
+}
