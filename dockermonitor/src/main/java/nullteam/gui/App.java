3ee3eefc2999240c76162c4c384b356b215cefab
Grigorios Alexandrou <t8220004@aueb.gr>
2023-12-22 21:21:48 +0200

    Katafera na prosarmosw ton kwdika tou argirh gia thn arxh tou gui

--- dockermonitor/src/main/java/nullteam/gui/App.java
+++ dockermonitor/src/main/java/nullteam/gui/App.java
@@ -1,18 +1,26 @@
 package nullteam.gui;
 
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class App extends Application {
+    /*
+    String sceneOne = "nullteam.gui/Scene1.fxml";
+    URL url  = null;
+
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("../../nullteam/gui/Scene1.fxml"));
+            URL url = Paths.get("dockermonitor/src/main/resources/Scene1.fxml").toUri().toURL();
+            root = FXMLLoader.load(url);
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -20,9 +28,7 @@ public class App extends Application {
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) throws Exception {
         launch(args);
     }
-
 }
