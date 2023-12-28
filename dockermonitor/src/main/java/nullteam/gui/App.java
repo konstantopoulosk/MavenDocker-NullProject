a97583279fb657b0ca6784eebf7a2788f3e96744
Grigorios Alexandrou <t8220004@aueb.gr>
2023-12-28 23:50:38 +0200

    Start

--- dockermonitor/src/main/java/nullteam/gui/App.java
+++ dockermonitor/src/main/java/nullteam/gui/App.java
@@ -1,5 +1,4 @@
 package nullteam.gui;
-
 import com.nullteam.ClientUpdater;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,52 +7,35 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 
 public class App extends Application {
-    /*
-     * String sceneOne = "nullteam.gui/Scene1.fxml";
-     * URL url = null;
-     * 
-     */
     static final int width = 1000;
     static final int height = 800;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         Parent root;
         try {
             root = FXMLLoader.load(getClass().getResource("/Scene1.fxml"));
             // Icon
-            FileInputStream inputStream = new FileInputStream("docker.png");
+            FileInputStream inputStream = new FileInputStream("Icon.png");
             Image icon = new Image(inputStream);
             // End Icon
             Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-
-
             primaryStage.getIcons().add(icon);
             primaryStage.setTitle("Docker Project");
             primaryStage.setResizable(false);
             primaryStage.setScene(scene);
-
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) throws Exception {
-        // ClientUpdater.connectionAccomplished();
-
+        //ClientUpdater.connectionAccomplished();
         launch(args);
-         Connection connection = ClientUpdater.connectToDatabase();
-
+        //Connection connection = ClientUpdater.connectToDatabase();
     }
 }
--- dockermonitor/src/main/java/nullteam/gui/MainSceneController.java
+++ dockermonitor/src/main/java/nullteam/gui/MainSceneController.java
@@ -30,76 +30,27 @@ public class MainSceneController {
 
     @FXML
     public void startApp(ActionEvent event) throws IOException  {
-        //todo next slide
-        System.out.println("Starting App");
-        //This is printed in the command line!
-
         //gia allagi skhnhs
+        System.out.println("Start!!!!");
         root = FXMLLoader.load(getClass().getResource("/Scene2.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
-
-
-
-        /*
-        Group rootMain = new Group();
-
-        //Title
-        Text title = new Text("Main Menu");
-        title.setX(50);
-        title.setY(50);
-        title.setFont(Font.font("Abyssinica SIL", FontWeight.BOLD, FontPosture.REGULAR,25));
-        title.setFill(Color.BLUE);
-        title.setStroke(Color.BLACK);
-        title.setStrokeWidth(1);
-        Reflection ref = new Reflection();
-        ref.setBottomOpacity(0.2);
-        ref.setFraction(12);
-        ref.setTopOffset(10);
-        ref.setTopOpacity(0.2);
-        title.setEffect(ref);
-        //End title
-
-        //Line under "Main Menu"
-        Line line = new Line();
-        line.setStartX(0);
-        line.setEndX(width);
-        line.setStartY(50);
-        line.setEndY(50);
-        //End Line under "Main Menu"
-
-         */
-        //Create another scene.fxml -> Name: MainMenu.fxml, style it!
-        //The above was a preview that I did with pure code.
-        //Use SceneBuilder it is much easier.
     }
     @FXML
     public void exitApp(ActionEvent event) {
         System.exit(0);
         //Exiting Application.
     }
-
-    public void backtoscene1(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/Scene1.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
     @FXML
-    public void SwitchToImages(ActionEvent event) throws IOException {
+    public void switchToImages(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/Scene4.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
     @FXML
     public void backtoscene2(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/Scene2.fxml"));
@@ -107,5 +58,21 @@ public class MainSceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    public void pressContainers(ActionEvent event) throws IOException {
+        System.out.println("Containers Pressed");
+    }
+    @FXML
+    public void pressLogs(ActionEvent event) throws IOException {
+        System.out.println("Logs Pressed");
+    }
+    @FXML
+    public void pressNetworks(ActionEvent event) throws IOException {
+        System.out.println("Networks Pressed");
+    }
+    @FXML
+    public void pressVolumes(ActionEvent event) throws IOException {
+        System.out.println("Volumes Pressed");
     }
 }
