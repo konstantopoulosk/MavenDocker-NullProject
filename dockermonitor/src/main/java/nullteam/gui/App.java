ca919811a1066f3092cbc4ba95f1f6589a01306f
Grigorios Alexandrou <t8220004@aueb.gr>
2024-01-15 13:22:58 +0200

    test lists done

--- dockermonitor/src/main/java/nullteam/gui/App.java
+++ dockermonitor/src/main/java/nullteam/gui/App.java
@@ -1,6 +1,7 @@
 package nullteam.gui;
 
 import com.nullteam.ClientUpdater;
+import com.nullteam.DatabaseThread;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
